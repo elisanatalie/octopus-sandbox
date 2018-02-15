a18408957550486d3252016605b33a618f3ed7cc
elisa.natalie <elisa.natalie@smaato.com>
2018-02-15 11:39:23 +0100

    Groovy string examples

--- src/main/groovy/org/stringops/SubstringExample.groovy
+++ src/main/groovy/org/stringops/SubstringExample.groovy
@@ -1,7 +1,13 @@
+import java.util.regex.Pattern
+
 class SubstringExample {
 
     static void main(String... args) {
-        def s = "origin/feature/DIT-28_B_TicketDescrption"
-        println "-" + s.minus("origin/feature/").toLowerCase().split('_')[0]
+        def s = "origin/feature/SCORE-28322_B_TicketDescrption"
+        def s1 = "-" + s.minus("origin/feature/").toLowerCase().find('[a-z]{1,5}-\\d{1,5}')
+        println 'device-id-targeting-s3-test-us-east-1' + s1
+        def compile = Pattern.compile("[a-zA-Z][-a-zA-Z0-9]*")
+
+        println compile.matcher('device-id-targeting-s3-test-us-east-1' + s1).matches()
     }
 }
