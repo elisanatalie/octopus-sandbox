0a6e8ad04145d977db79b19efa7e7db41807fb80
elisa.natalie <elisa.natalie@smaato.com>
2018-04-06 15:26:43 +0200

    Phaser example

---
+++ src/main/java/org/phaser/LockStepCyclicBarrier.java
@@ -0,0 +1,42 @@
+package org.phaser;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+
+public class LockStepCyclicBarrier extends LockStepExample {
+
+    public static void main(String... args) {
+        LockStepCyclicBarrier lse = new LockStepCyclicBarrier();
+
+        ExecutorService pool = newFixedThreadPool(TASKS_PER_BATCH);
+        CyclicBarrier barrier = new CyclicBarrier(TASKS_PER_BATCH);
+
+        for (int batch = 0; batch < BATCHES; batch++) {
+            for (int i = 0; i < TASKS_PER_BATCH; i++) {
+                int batchNumber = batch + 1;
+                pool.submit(() -> lse.task(barrier, batchNumber));
+            }
+        }
+        pool.shutdown();
+    }
+
+    public void task(CyclicBarrier barrier,
+                     int batch) {
+        boolean interrupted = Thread.interrupted();
+        while (true) {
+            try {
+                barrier.await();
+                break;
+            } catch (InterruptedException e) {
+                interrupted = true;
+            } catch (BrokenBarrierException e) {
+                throw new AssertionError(e);
+            }
+        }
+        if (interrupted) Thread.currentThread().interrupt();
+        doTask(batch);
+    }
+}
---
+++ src/main/java/org/phaser/LockStepPhaser.java
@@ -0,0 +1,28 @@
+package org.phaser;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Phaser;
+
+public class LockStepPhaser extends LockStepExample {
+
+    public static void main(String... args) {
+        LockStepPhaser lse = new LockStepPhaser();
+
+        ExecutorService pool = newFixedThreadPool(TASKS_PER_BATCH);
+        Phaser phaser = new Phaser(TASKS_PER_BATCH);
+
+        for (int batch = 0; batch < BATCHES; batch++) {
+            for (int i = 0; i < TASKS_PER_BATCH; i++) {
+                pool.submit(() -> lse.task(phaser));
+            }
+        }
+        pool.shutdown();
+    }
+
+    public void task(Phaser phaser) {
+        phaser.arriveAndAwaitAdvance();
+        doTask(phaser.getPhase());
+    }
+}
